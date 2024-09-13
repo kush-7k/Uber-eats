d856bacdbe6b15bb510a2a21ba7e998fa22d28bd
Kush Patel <113313092+kush-7k@users.noreply.github.com>
2024-09-13 14:49:07 -0400

    Complete Frontend

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>Order food online or in the Uber Eats app and support local restaurants. Find the best restaurants that deliver. Get contactless delivery for restaurant takeout, groceries, and more!</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
