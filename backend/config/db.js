d43c8d2a637629136735b3df72059c0012937fd8
Kush Patel <113313092+kush-7k@users.noreply.github.com>
2024-10-09 00:57:29 -0400

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://kushp777:Kush123@cluster0.11gpp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('YOur MONGODB DATABASE CONNECTION STRING').then(()=>console.log("DB Connected"));
    
 }
 
